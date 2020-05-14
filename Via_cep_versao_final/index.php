e275db599cc34cc7816cc335acf01ff30d9fdfb9
Redkiller999 <edy_madureira@hotmail.com>
2020-05-13 22:57:01 -0300

    Add files via upload

--- Via_cep_versao_final/index.php
+++ Via_cep_versao_final/index.php
@@ -1,25 +1,26 @@
 <?php
-        include_once ('viacep.php');
+	include_once ('viacep.php');
+    $address = getAddress();
+    
+
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
+ <! DOCTYPE html>
+<html  lang = "pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consumindo API</title>
-    <link rel="stylesheet" href="style.css">
+    <meta  charset = "UTF-8" >
+    <meta  name = "viewport"  content = " width = largura do dispositivo, escala inicial = 1.0 " >
+     <title> Consumindo </title>
+    <link  rel = "stylesheet"  href = "style.css" >
 </head>
 <body>
-    <form action="." method="post">
-        <p>Digite o CEP para encontrar o endereço.</p>
-        <input type="text" placeholder="Digite um cep..." name="cep">
-        <input type="submit">
-        <input type="text" placeholder="rua" name="rua" value="<?php echo $address->logradouro ?>">
-        <input type="text" placeholder="bairro" name="bairro" value="<?php echo $address->bairro ?>">
-        <input type="text" placeholder="cidade" name="cidade" value="<?php echo $address->localidade ?>">
-        <input type="text" placeholder="estado" name="estado" value="<?php echo $address->uf ?>">
-    </form>
+    <form  action = "."  method = "post">
+        <p> Digite o CEP para encontrar o endereço. </p>
+        <input type = "text"  espaço reservado = " Digite um cep ... "  name = "cep" <?php echo $address->cep ?>>
+        <input type = "submit"><br>
+        <input type = "text" espaço reservado = "rua"  name = "rua" value = "<?php echo $address-> logradouro ?>"><br>
+        <input type = "text" espaço reservado = "bairro" name = "bairro" value = <?php echo $address-> bairro ?>><br>
+        <input type = "text" espaço reservado = "cidade " name = "cidade" value = <?php echo $address-> localidade ?>><br>
+        <input type = "text" espaço reservado = "estado " name = "estado" value = <?php echo $address-> uf ?>>
+    </form >
 </body>
 </html>
-
- 
--- Via_cep_versao_final/viacep.php
+++ Via_cep_versao_final/viacep.php
@@ -1,33 +1,60 @@
-<?php 
-
-$address = (object) array (
-    'cep' => '',
-    'logradouro' => '',
-    'bairro' => '',
-    'localidade' => '',
-    'uf' => ''
-);
-     
-
-
-
-
-
-if ( isset ($_POST['cep'])){
-
-    $cep = $_POST['cep'];
-    $cep = preg_replace('/[^0-9]/','',$cep);
-   
+<?php
+function getAddress(){    
+    
 
     
-    if ( preg_match ('/^[0-9]{5}-?[0-9]{3}$/',$cep) ){
-        $url = "http://viacep.com.br/ws/{$cep}/json/";
+    if (isset ($_POST['cep'])){
+        $cep = $_POST['cep'];
+      
 
+        $cep = filterCep ($cep);
 
-        $address = json_decode(file_get_contents($url)); 
-    } else {
-        $address->cep = 'CEP Inválido!';
+        if(isCep($cep)){
+
+            $address = getAddressViaCep($cep);
+            if(property_exists($address,'erro')){
+                 $address = addressEmpty();
+                 $address->cep = 'CEP não encontrado!';
+                
+            }
+       
+        }
+
+    }else{
+        
+        $address = addressEmpty();
+        
     }
+    
+    return $address;
+    
 }
 
+function addressEmpty(){
+    return (object)[
+        'cep'=> '',
+        'logradouro'=> '',
+        'bairro'=> '',
+        'localidade'=>'',
+        'uf'=> ''
 
+    ];
+    
+    
+}
+
+function filterCep (String $cep):String{
+    return preg_replace('/[^0-9]/','',$cep);
+    
+}
+
+function isCep(String $cep):bool{
+    return preg_match('/^[0-9]{5}-?[0-9]{3}$/',$cep);
+    
+}
+
+function getAddressViaCep (String $cep) {
+    $url = "viacep.com.br/ws/{$cep}/json/";
+    return json_decode(file_get_contents($url));
+    
+}
